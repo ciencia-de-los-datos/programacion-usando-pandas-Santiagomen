564ed1e6b5c04a43c355cf2c830b1a8d06ef90bc
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2022-10-07 19:31:23 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,12 @@ def pregunta_01():
     40
 
     """
-    return
+    import pandas as pd
+    tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
+    tbl1 = pd.read_csv("tbl1.tsv", sep="\t")
+    tbl2 = pd.read_csv("tbl2.tsv", sep="\t")
+    
+    return len(tbl0)
 
 
 def pregunta_02():
